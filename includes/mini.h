a9b3d86e2dad8f717e08ebcf228b862243d1fd89
Diego Moreira Sardinha De Souza <diegoaguia31@gmail.com>
2024-02-20 12:54:34 +0000

    (update) put libft

--- includes/mini.h
+++ includes/mini.h
@@ -1,5 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 12:40:37 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/20 12:40:39 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "src/libft/libft.h"
+
 
 typedef struct pipe
 {
@@ -11,3 +25,15 @@ typedef struct pipe
 	struct pipe *next;
 	
 }				t_pipe;
+
+typedef struct words
+{
+	char *value;
+	int flag;
+} t_words;
+
+typedef struct words_containt
+{
+	t_words *next;
+
+};
