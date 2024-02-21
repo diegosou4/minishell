d52d76672be3d25176d864b4f000f5909b9d6632
Juan Pablo Martinez Aldana <juan.martinez43@udea.edu.co>
2024-02-21 09:08:49 +0000

    (update) organize the file as norminette structure

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-pma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 08:59:54 by juan-pma          #+#    #+#             */
+/*   Updated: 2024/02/21 09:05:41 by juan-pma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/mini.h"
 
 /*
     1. check the access fucntion to get the path
     of te process.
-
-*/
-
-int main(int argc, char *argv[],char **env) 
-{
-    /*
-        1. We can add the signal hanlder, in this part.
+    2. we can add the signal hanlder, in this part.
     */
-   if (argc == 1 && argv[0])
-   {
-    ft_parse_manager(env);
-   }
+
+int	main(int argc, char *argv[], char **env)
+{
+	if (argc == 1 && argv[0])
+		ft_parse_manager(env);
 }
