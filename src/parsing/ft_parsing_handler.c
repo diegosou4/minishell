4273567f9f0565f3196adbc3d6c28efa6eafac68
Juan Pablo Martinez Aldana <juan.martinez43@udea.edu.co>
2024-02-21 15:08:22 +0000

    (update) change in the special case function to avoid <>

--- src/parsing/ft_parsing_handler.c
+++ src/parsing/ft_parsing_handler.c
@@ -93,7 +93,9 @@ int ft_parse_handler(char *str, const char *delimiters)
 char *ft_create_string(char *line)
 {
     char *new_line;
-    
+    int i;
+
+    i = 0;
     new_line = ft_calloc(ft_strlen(line), sizeof(char*));
     new_line = ft_string_handle(line, new_line);
     if (new_line)
