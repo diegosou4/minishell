501f01e5b4ec3c04d8d638d3a44c00ccaa7921bf
Juan Pablo Martinez Aldana <juan.martinez43@udea.edu.co>
2024-02-21 09:45:33 +0000

    (update) update the norminette rules for the file

--- src/parsing/ft_parse_manager.c
+++ src/parsing/ft_parse_manager.c
@@ -1,27 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_manager.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-pma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 09:42:35 by juan-pma          #+#    #+#             */
+/*   Updated: 2024/02/21 09:44:44 by juan-pma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/mini.h"
 
-
-
-void *ft_parse_manager(char **env)
+void	*ft_parse_manager(char **env)
 {
-    char *line;
-    // char *new_line;
+	char	*line;
+	char	*new_line;
+	char	*path;
 
-    char *path;
-
-    path = ft_getenv(env);
-    printf("Paths %s \n",path);
-    while (1)
-    {
-        line = readline("🐧>shell:$");
-        // Check for Ctrl+D (EOF) to exit the loop
-        if (!line)
-            break;
-        // Create the line history. 
-        add_history(line);
-        ft_create_string(line);
-        // recieve the line, that was readed.
-        free(line);
-    }
-    return NULL;
+	path = ft_getenv(env);
+	printf("Paths %s \n", path);
+	while (1)
+	{
+		line = readline("🐧>shell:$");
+		if (!line)
+			break ;
+		add_history(line);
+		ft_create_string(line);
+		free(line);
+	}
+	return (NULL);
 }
