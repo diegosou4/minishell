82aaafce845447a8859bafb5b55ae5970e3d1b7e
diegosou4 <diegoaguia31@gmail.com>
2024-02-22 11:43:14 +0000

    (update) refactor ft_arrcpy

--- src/parsing/ft_expand.c
+++ src/parsing/ft_expand.c
@@ -47,38 +47,48 @@ static char *ft_strcpy(char *dest, const char *src)
         dest++;
         src++;
     }
-    *dest = '\0';
     return dest;
 }
+
+
+static void	*free_malloc(char **s, int i)
+{
+	while (i >= 0)
+	{
+		free(s[i]);
+		i--;
+	}
+	free(s);
+	return (NULL);
+}
+
 
 char **ft_arrcpy(char **str) 
 {
     int i = 0;
     int j = 0;
+    int len;
     char **str_copy ;
     if (!str || !str[0])
         return NULL;    
     while (str[i]) 
         i++;    
-    str_copy = (char **)malloc((i + 1) * sizeof(char *));
+    str_copy = (char **)ft_calloc((i + 1) , sizeof(char *));
     if (!str_copy) 
         return NULL;
     while (j < i) 
     {
-        int len = strlen(str[j]);
-        str_copy[j] = (char *)malloc((len + 1) * sizeof(char));
+        len = strlen(str[j]);
+        str_copy[j] = (char *)ft_calloc((len + 1) , sizeof(char));
         if (!str_copy[j]) {
-            // memory allocation failed, free previously allocated memory
-            for (int k = 0; k < j; k++) {
-                free(str_copy[k]);
-            }
-            free(str_copy);
-            return NULL;
+            return(free_malloc(str_copy,j));
         }
         ft_strcpy(str_copy[j], str[j]);
         j++;
     }
     return(str_copy);
 }
+
+
 
 
