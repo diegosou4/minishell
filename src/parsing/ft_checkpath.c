ccec6477a17c6d4750983c45c33ff74ea49de0be
Juan Aldana <juan.martinez43@udea.edu.co>
2024-02-22 11:12:45 +0000

    (update) check the path

--- src/parsing/ft_checkpath.c
+++ src/parsing/ft_checkpath.c
@@ -12,6 +12,19 @@
 
 #include "../../includes/mini.h"
 
+static void	free_paths(char **paths)
+{
+	int	i;
+
+	i = 0;
+	while (paths[i] != NULL)
+	{
+		free(paths[i]);
+		i++;
+	}
+	free(paths);
+}
+
 char	*ask_acess(char *comand, char *path)
 {
 	char	**paths;
@@ -25,8 +38,6 @@ char	*ask_acess(char *comand, char *path)
 		cplusp = ft_strjoin(paths[i], comand);
 		if (access(cplusp, F_OK) == 0)
 		{
-			free(cplusp);
-			cplusp = ft_strdup(paths[i]);
 			free_paths(paths);
 			return (cplusp);
 		}
