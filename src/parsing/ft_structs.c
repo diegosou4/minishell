3ce752ae5ee97c4c82da470fd34d2b317e296023
diegosou4 <diegoaguia31@gmail.com>
2024-02-22 20:10:26 +0000

    (update) comment fuction

--- src/parsing/ft_structs.c
+++ src/parsing/ft_structs.c
@@ -20,11 +20,11 @@ t_redir *redirnew(void)
     redir = (t_redir *) ft_calloc(sizeof(t_redir),1);
     if(redir == NULL)
         return NULL;
-    redir->str = NULL;
+    redir->pathout = NULL;
+    redir->pathin = NULL;
     redir->in = -1;
     redir->out = -1;
     redir->append = 0;
-    redir->next = NULL;
     return(redir);
 }
 
