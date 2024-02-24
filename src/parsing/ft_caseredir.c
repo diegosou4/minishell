c8c21ee41fb23f2ffade8ed0a903f3485da2476f
diegosou4 <diegoaguia31@gmail.com>
2024-02-24 14:28:13 +0000

    (update) we not open the files in parsing, only in exec

--- src/parsing/ft_caseredir.c
+++ src/parsing/ft_caseredir.c
@@ -15,39 +15,14 @@
 
 void checkpathredir(t_redir *redir, char *file, int flag)
 {
+    redir->token = flag;
     if(flag == 1)
     {
-        if(redir->pathin  != NULL)
-            free(redir->pathin);
         redir->pathin = ft_strdup(file);
     }
     if(flag == 2 || flag == 4)
     {
-        if(redir->pathout  != NULL)
-            free(redir->pathout);
         redir->pathout = ft_strdup(file);
     }
 
-}
-
-void checkinoutredir(t_redir *redir, char *file, int flag)
-{
-    if(flag == 1)
-    {
-        if(redir->out >= 3)
-            close(redir->out);
-        redir->out = open(file, O_WRONLY | O_CREAT | O_TRUNC);
-    }
-    if(flag == 2)
-    {
-       if(redir->in >= 3)
-            close(redir->in);
-        redir->in = open(file, O_RDONLY);;
-    }
-    if(flag == 5)
-    {
-        if(redir->out >= 3)
-            close(redir->out);
-        redir->out = open(file, O_WRONLY | O_CREAT | O_APPEND);
-    }
 }
