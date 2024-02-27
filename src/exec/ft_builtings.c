68e2bfb179e43b12cfa43c910d3d58e2be3a1b39
diegosou4 <diegoaguia31@gmail.com>
2024-02-27 18:01:48 +0000

    (add)m fuctions for exec the line

---
+++ src/exec/ft_builtings.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_builtings.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 11:08:24 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/27 11:08:25 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/mini.h"
+
+int check_builtings(t_cmd *commands, char **env)
+{
+    if(ft_strncmp("pwd",commands->args[0],4) == 0)
+        return(1);
+    else if (ft_strncmp("cd",commands->args[0],2) == 0)
+    {
+        return(2);
+    }
+    else if(ft_strncmp("env",commands->args[0],3) == 0)
+        return(3);
+    /*
+    if(ft_strncmp("echo",commands->args[0],5) == 0)
+        //ft_echo()
+  
+    
+        //ft_pwd
+    else if(ft_strncmp("export",commands->args[0],6) == 0)
+        //ft_export()
+    else if(ft_strncmp("unset", commands->args[0],5) == 0)
+        //ft_export()
+    else if(ft_strncmp("exit", commands->args[0],5) == 0)
+        //ft_exit
+    */
+    return(0);
+    }
---
+++ src/exec/ft_execution.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execution.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 13:28:19 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/26 13:28:20 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/mini.h"
+
+void execute_cmd(t_cmd *commands, char **env)
+{
+    t_redir *ptr;
+    int in;
+    int out;
+    in = 0;
+    out = 1;
+    int fd[2];
+
+    pipe(fd);
+    openredir(commands->redir);
+    ptr = commands->redir;
+    if(ptr != NULL)
+    {
+    while(ptr->next)
+    {
+        if(ptr->token == 2)
+        {
+            if(in != 1)
+                close(in);
+            else
+                in = dup(ptr->fd);
+        }
+        ptr = ptr->next;
+    }
+    }
+ 
+
+}
+
+void exec_simple(t_cmd *commands,char **env)
+{
+    int pid;
+    pid = fork();
+    if(pid == 0)
+    {
+    execve(commands->path,commands->args,env);
+    }
+    wait(NULL);
+}   
+void execute_simple(t_cmd *commands, char **env)
+{
+    if(sizeredir(commands->redir) == 0)
+    {
+      //  check_builtings(commands);
+        exec_simple(commands,env);
+        free_commands(commands);
+    }
+}
+
+void execution(t_cmd *commands, char **env)
+{
+    t_cmd *ptr;
+
+    ptr = commands;
+    if(sizepipe(commands) == 1)
+    {
+    if(check_builtings(commands,env) == 1)
+        execute_pwd(env,commands);
+    else if(check_builtings(commands,env) == 2)
+        ft_cd(env,commands);  
+    else if(check_builtings(commands,env) == 3)
+        execute_env(env,commands);
+    else if(check_builtings(commands,env) == 0)
+    {
+        int pid = fork();
+        if(pid == 0)
+        {
+        execve(commands->path,commands->args,env);
+        }
+    }
+    }
+}
+
+int sizeredir(t_redir *redir)
+{
+    int i;
+    i = 0;
+
+    t_redir *ptr;
+    
+    ptr = redir;
+    while(ptr != NULL)
+    {
+        i++;
+        ptr = ptr->next;
+    }
+    return(i);
+
+}
+
+
+int sizepipe(t_cmd *commands)
+{
+    int i;
+    i = 0;
+
+    t_cmd *ptr;
+
+    ptr = commands;
+    while(ptr != NULL)
+    {
+        i++;
+        ptr = ptr->next;
+    }
+    return(i);
+}
