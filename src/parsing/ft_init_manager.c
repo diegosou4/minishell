b3ec18db911dc9b714a7c9286354b334317b95b9
Juan Aldana <juan.martinez43@udea.edu.co>
2024-02-27 18:43:47 +0000

    (update) remove the unused function

--- src/parsing/ft_init_manager.c
+++ src/parsing/ft_init_manager.c
@@ -1,10 +1,2 @@
 #include "../../includes/mini.h"
 
-void ft_init_operations(t_operations *operations)
-{
-    operations->apppend_out = ">>";
-    operations->pipe = "|";
-    operations->redir_input = "<";
-    operations->redir_output = ">";
-    operations->here_doc = "<<";
-}
