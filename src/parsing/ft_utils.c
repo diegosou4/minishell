8201e421377f34e0539c739f98ffd1963304cc39
Juan Aldana <juan.martinez43@udea.edu.co>
2024-02-27 18:34:09 +0000

    (update) update the functions and add the whitespace and the strtok to create the tokens

--- src/parsing/ft_utils.c
+++ src/parsing/ft_utils.c
@@ -14,6 +14,10 @@ char *ft_strcpy(char *dest, const char *src);
 
 #include "../../includes/mini.h"
 
+/*
+    🚩 check this function as we are alocating 
+    memmory here.
+*/
 t_cmd *cmdnew(char *args)
 {
 	t_cmd *comands;
@@ -25,14 +29,14 @@ t_cmd *cmdnew(char *args)
     len = 0;
 	comands = (t_cmd *)ft_calloc(sizeof(t_cmd), 1);
     newarg = ft_split(args, '2');
+
     while (newarg[len] != NULL)
         len++;
     char **modified_list = (char **)malloc((len * 5) * sizeof(char *));
     while (++i < len)
     {
-        modified_list[i] = (char *)malloc((strlen(newarg[i]) + 1) * sizeof(char));    
+        modified_list[i] = (char *)malloc((ft_strlen(newarg[i]) + 1) * sizeof(char));    
         ft_strcpy(modified_list[i], newarg[i]);
-        ft_checker_quotes(modified_list[i], comands);
     }
     comands->args = modified_list;
 	comands->next = NULL;
@@ -70,7 +74,7 @@ char *ft_strcpy(char *dest, const char *src)
     return dest;
 }
 
-void ft_checker_quotes(char *str, t_cmd *structure)
+void ft_checker_quotes(char *str)
 {
     int len;
 
@@ -78,16 +82,51 @@ void ft_checker_quotes(char *str, t_cmd *structure)
     if (str[0] == '\"' && ft_strchr((const char *) str, '$'))
     {
         printf("variable found we need to expand :%s: \n", str);
-        structure->fint_variable = TRUE;
-
     }
     if ((str[0] == '\'' && str[len - 1] == '\'')
         || (str[0] == '\"' && str[len - 1] == '\"'))
     {
         str[len - 1] = '\0';
-        structure->literal = TRUE;
         ft_memmove(str, (str + 1), len - 1);
     }
     else if (ft_strchr((const char *) str, '$'))
-        structure->fint_variable = TRUE;
+        printf("var find \n");
+}
+
+char	*ft_strtok(char *str, const char *delimiters)
+{
+	static char	*buffer = NULL;
+	char		*token_start;
+
+	if (!str && !buffer)
+		return (NULL);
+
+	if (str)
+		buffer = str;
+
+	while (*buffer && ft_strchr(delimiters, *buffer))
+		buffer++;
+
+	token_start = buffer;
+
+	buffer = ft_strpbrk(token_start, delimiters);
+	if (buffer)
+		*buffer++ = '\0';
+	return (token_start);
+}
+
+int ft_whitespace(char *line)
+{
+    int i;
+
+    i = 0;
+    
+    while ((line[i] >= '\b' && line[i] <= '\v') || line[i] == ' ')
+    {
+        i++;
+    }
+    if (i == (int) ft_strlen(line))
+        return (0);
+        
+    return (1);
 }
