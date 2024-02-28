f0f2e9c49c1ece2a8d62a00159797fb94ff10672
diegosou4 <diegoaguia31@gmail.com>
2024-02-28 14:24:52 +0000

    (add) new files for buildings

---
+++ src/libft/ft_strrjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrjoin.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 11:06:55 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/28 11:06:56 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+
+char    **ft_strrjoin(char **arr, char *str)
+{
+    char **newstr;
+    int i;
+
+    i = len_darray(arr) + 2;
+
+    newstr = ft_calloc(sizeof(char *), i);
+
+    int j;
+    j = 0;
+    while(j != (i - 3))
+    {
+        newstr[j] = ft_strdup(arr[j]);
+        j++;
+    }
+    i = j;
+    newstr[j++] = ft_strdup(str);
+    newstr[j++] = ft_strdup(arr[i]);
+    freedouble_malloc(arr,len_darray(arr));
+    return(newstr);
+}
---
+++ src/libft/ft_strstrjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstrstrjoin.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 13:57:33 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/28 13:57:34 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strstrjoin(char *s1, char *s2, char s3,char *s4)
+{
+    char *new_str;
+    int len;
+    int j = 0;
+
+    len = ft_strlen(s1) + ft_strlen(s2);
+    len += ft_strlen(s4) + 2; 
+    new_str = ft_calloc(len, sizeof(char));
+
+    while(*s1 != '\0')
+        new_str[j++] = *s1++;
+    while(*s2 != '\0')
+        new_str[j++] = *s2++;
+    new_str[j++] = s3;
+    while(*s4 != '\0')
+        new_str[j++] = *s4++;
+    return (new_str);
+}
