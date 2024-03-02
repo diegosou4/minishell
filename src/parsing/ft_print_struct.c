857acb3f4c9039efab56eb82713fca5c81de5768
Juan Aldana <juan.martinez43@udea.edu.co>
2024-03-02 15:28:11 +0000

    (update) update the print structure to have more visualization

--- src/parsing/ft_print_struct.c
+++ src/parsing/ft_print_struct.c
@@ -52,14 +52,15 @@ void ft_print_doble_char(char **argv)
 
 void ft_print_list_struct(t_word_list *structure)
 {
-	printf (ANSI_COLOR_GREEN"____________START__STRUCT____________\n"ANSI_COLOR_RESET);
-	while (structure)
-	{
-		printf(ANSI_COLOR_YELLOW "this is the word " ANSI_COLOR_RESET ANSI_COLOR_GREEN ":%s:\n" ANSI_COLOR_RESET, structure->word->word);
-		// printf(ANSI_COLOR_RED "this is the Flag " ANSI_COLOR_RESET ANSI_COLOR_GREEN ":%d:\n", structure->word->flags);
-		// printf(ANSI_COLOR_RED "this is the Tag  " ANSI_COLOR_RESET ANSI_COLOR_GREEN ":%d:\n", structure->word->tags);
+    printf(ANSI_COLOR_GREEN "____________START__STRUCT____________\n" ANSI_COLOR_RESET);
+    while (structure)
+    {
+        printf(ANSI_COLOR_YELLOW "Word: " ANSI_COLOR_RESET ANSI_COLOR_GREEN " %s\n" ANSI_COLOR_RESET, structure->word->word);
+        printf(ANSI_COLOR_RED "Flags: " ANSI_COLOR_RESET ANSI_COLOR_GREEN "%d\n" ANSI_COLOR_RESET, structure->word->flags);
+        printf(ANSI_COLOR_RED "Tags: " ANSI_COLOR_RESET ANSI_COLOR_GREEN "%d\n" ANSI_COLOR_RESET, structure->word->tags);
+        printf("------------------------------------\n");
 
-		structure = structure->next;
-	}
-	printf (ANSI_COLOR_CYAN"____________END__STRUCT____________\n\n"ANSI_COLOR_RESET);
+        structure = structure->next;
+    }
+    printf(ANSI_COLOR_CYAN "____________END__STRUCT____________\n\n" ANSI_COLOR_RESET);
 }
