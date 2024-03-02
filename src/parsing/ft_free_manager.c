3b53b8586fa657e6f95c0c4162b2476ecb6695f7
Juan Aldana <juan.martinez43@udea.edu.co>
2024-03-02 15:29:21 +0000

    (add) create a function to free the memmory of the strucure t_env

--- src/parsing/ft_free_manager.c
+++ src/parsing/ft_free_manager.c
@@ -11,3 +11,17 @@ void ft_free_double_pointers(char **split_line)
         free(split_line[i]);
     free(split_line);
 }
+void ft_free_env_list(t_env *env)
+{
+    t_env *current;
+    t_env *temp;
+
+    current = env;
+    while (current != NULL)
+    {
+        temp = current;
+        current = current->next;
+        free(temp->key);
+        free(temp);
+    }
+}
