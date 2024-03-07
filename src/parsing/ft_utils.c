1c0bdb843958ed0828dc640343a49a33b68b73e7
Juan Aldana <juan.martinez43@udea.edu.co>
2024-03-07 17:39:17 +0000

    (update) the error closing quotes handler

--- src/parsing/ft_utils.c
+++ src/parsing/ft_utils.c
@@ -18,47 +18,47 @@ char *ft_strcpy(char *dest, const char *src);
     🚩 check this function as we are alocating
     memmory here.
 */
-t_cmd *cmdnew(char *args)
-{
-    t_cmd *comands;
-    char **newarg;
-    int len;
-    int i;
+// t_cmd *cmdnew(char *args)
+// {
+//     t_cmd *comands;
+//     // char **newarg;
+//     int len;
+//     int i;
 
-    i = -1;
-    len = 0;
-    comands = (t_cmd *)ft_calloc(sizeof(t_cmd), 1);
-    newarg = ft_split(args, '2');
+//     i = -1;
+//     len = 0;
+//     comands = (t_cmd *)ft_calloc(sizeof(t_cmd), 1);
+//     // newarg = ft_split(args, '2');
 
-    while (newarg[len] != NULL)
-        len++;
-    char **modified_list = (char **)malloc((len * 5) * sizeof(char *));
-    while (++i < len)
-    {
-        modified_list[i] = (char *)malloc((ft_strlen(newarg[i]) + 1) * sizeof(char));
-        ft_strcpy(modified_list[i], newarg[i]);
-    }
-    comands->args = modified_list;
-    comands->next = NULL;
-    comands->redir = NULL;
-    return (comands);
-}
+//     // while (newarg[len] != NULL)
+//     //     len++;
+//     // char **modified_list = (char **)malloc((len * 5) * sizeof(char *));
+//     // while (++i < len)
+//     // {
+//     //     modified_list[i] = (char *)malloc((ft_strlen(newarg[i]) + 1) * sizeof(char));
+//     //     ft_strcpy(modified_list[i], newarg[i]);
+//     // }
+//     comands->args = modified_list;
+//     comands->next = NULL;
+//     comands->redir = NULL;
+//     return (comands);
+// }
 
-void cmdinback(t_cmd **comands, char *args)
-{
-    t_cmd *ptr;
-    t_cmd *last;
+// void cmdinback(t_cmd **comands, char *args)
+// {
+//     t_cmd *ptr;
+//     t_cmd *last;
 
-    last = cmdnew(args);
-    if (last == NULL)
-        return;
-    ptr = *(comands);
-    while (ptr->next != NULL)
-    {
-        ptr = ptr->next;
-    }
-    ptr->next = last;
-}
+//     last = cmdnew(args);
+//     if (last == NULL)
+//         return;
+//     ptr = *(comands);
+//     while (ptr->next != NULL)
+//     {
+//         ptr = ptr->next;
+//     }
+//     ptr->next = last;
+// }
 char *ft_strcpy(char *dest, const char *src)
 {
     if (!src)
@@ -82,52 +82,10 @@ static void ft_preline(char *line)
         if (line[i] == '$' && line[i + 1] == '\'')
         {
             ft_memmove(&line[i], &line[i + 1], ft_strlen(&line[i + 1]) + 1);
-            // ft_checker_quotes(&line[i], 2);
         }
     }
     // printf("line without $ :%s:\n", line);
 }
-
-// void ft_checker_quotes(char *str)
-// {
-//     int len;
-//     len = ft_strlen(str);
-
-//     while (*str)
-//     {
-//         if ((str[0] == '\'' && str[len - 1] == '\'') || (str[0] == '\"' && str[len - 1] == '\"'))
-//         {
-//             str[len - 1] = '\0';
-//             ft_memmove(str, (str + 1), len - 1);
-//             break;
-//         }
-//     }
-// }
-
-// void ft_checker_quotes_single(char *str)
-// {
-//     char *src;
-//     char *dest;
-//     int flag;
-
-//     flag = 0;
-//     src = str;
-//     dest = str;
-
-//     while (*src)
-//     {
-//         if (*src == '\'' && !flag)
-//         {
-//             src++;
-//             continue;
-//         }
-//         if (*src == '\'' && flag)
-//             flag = !flag;
-//         *dest++ = *src++;
-//     }
-//     *dest = '\0';
-//     printf("this is the dest :%s:\n", dest);
-// }
 
 void ft_checker_double_single(char *str, char quote)
 {
@@ -151,7 +109,7 @@ void ft_checker_double_single(char *str, char quote)
         *dest++ = *src++;
     }
     *dest = '\0';
-    printf("this is the dest :%s:\n", dest);
+    // printf("this is the dest :%s:\n", dest);
 }
 void ft_checker_quotes(char *str)
 {
@@ -175,7 +133,7 @@ void ft_checker_quotes(char *str)
         }
         src++;
     }
-    printf("this is the dest :%s:\n", src);
+    // printf("this is the dest :%s:\n", src);
 }
 
 void ft_quotes_remove(t_word_list *word_list)
