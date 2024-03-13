77b0eccd438f99c539b16d51ff2b8ada818d5d12
martinzx13 <juan.martinxz43@gmail.com>
2024-03-13 14:03:26 +0000

    (update) add the function to hanlde all the redir cases

--- src/parsing/ft_parsing_handler.c
+++ src/parsing/ft_parsing_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parsing_handler.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juan-pma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:44:08 by juan-pma          #+#    #+#             */
-/*   Updated: 2024/02/21 19:55:24 by juan-pma         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:50:33 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ static int ft_check_close_q(const char *ptr)
 }
 static int ft_check_close(const char *ptr)
 {
-	printf("this is the line quote :%s:\n", ptr);
 	char flag;
 
 	flag = 0;
@@ -54,75 +53,90 @@ static int ft_check_close(const char *ptr)
 	}
 	return (0);
 }
+static int ft_space (char **line)
+{
+    while(**line == '2')
+        (*line)++;
+    return (1);
+}
+
+static char *ft_string_handle_2(char *line, char *modified_line)
+{
+	int j;
+	char flag_quotes;
+
+	flag_quotes = 0;
+	j = 0;
+	while (*line)
+	{
+		if (flag_quotes == 0 && (*line == '\"' || *line == '\''))
+			flag_quotes = *line;
+		else if (flag_quotes == *line)
+			flag_quotes = 0;
+		if (flag_quotes == 0 && (*line == '<' || *line == '>'))
+		{
+			modified_line[j++] = '2';
+			while (*line == '<' || *line == '>')
+			{
+				modified_line[j++] = *line;
+				line++;
+			}
+			ft_space(&line);
+			if (*line == '<' ||  *line == '>')
+				return NULL;
+			modified_line[j++] = '2';
+			flag_quotes = *line;
+		}
+		modified_line[j++] = *line;
+		line++;
+	}
+	modified_line[j] = '\0';
+	return (modified_line);
+}
 
 static char *ft_string_handle(char *line, char *modified_line)
 {
 	int j;
 	char flag;
-	char *ptr;
 
-	ptr = line;
 	flag = 0;
 	j = 0;
-	while (*ptr)
+	while (*line)
 	{
-		if (flag == 0 && (*ptr == '\"' || *ptr == '\''))
-		{
-			flag = *ptr;
-		}
-		else if (flag == *ptr)
+		if (flag == 0 && (*line == '\"' || *line == '\''))
+			flag = *line;
+		else if (flag == *line)
 			flag = 0;
-		if (flag == 0 && *ptr == ' ')
-			*ptr = '2';
-		else if (flag == 0)
-		{
-			if ((j = ft_special_case(modified_line, j, &ptr)) == -2)
-				return (NULL);
-		}
-		if (flag == 0 && *ptr == '|')
-			*ptr = '3';
-		modified_line[j++] = *ptr;
-		ptr++;
+		if (flag == 0 && *line == ' ')
+			*line = '2';
+		if (flag == 0 && *line == '|')
+			*line = '3';
+		modified_line[j++] = *line;
+		line++;
 	}
+	modified_line[j] = '\0';
 	return (modified_line);
 }
-// static char *ft_string_handle(char *line, char *modified_line)
-// {
-// 	int j;
-// 	char flag;
-
-// 	flag = 0;
-// 	j = 0;
-// 	while (*line)
-// 	{
-// 		if (flag == 0 && (*line == '\"' || *line == '\''))
-// 			flag = *line;
-// 		else if (flag == *line)
-// 			flag = 0;
-// 		if (flag == 0 && *line == ' ')
-// 			*line = '2';
-// 		else if (flag == 0)
-// 		{
-// 			if ((j = ft_special_case(modified_line, j, &line)) == -2)
-// 				return (NULL);
-// 		}
-// 		if (flag == 0 && *line == '|')
-// 			*line = '3';
-// 		modified_line[j++] = *line;
-// 		line++;
-// 	}
-// 	return (modified_line);
-// }
 
 char *ft_create_string(char *line)
 {
 	char *new_line;
+	char *new_line_2;
 
 	new_line = ft_calloc(ft_strlen(line), sizeof(char *));
-	new_line = ft_string_handle(line, new_line);
-	printf("this is the line :%s:\n", new_line);
-	if (ft_check_close(new_line) == 1)
-		new_line = NULL;
-
-	return (new_line);
+	new_line_2 = ft_calloc(ft_strlen(new_line), sizeof(char *));
+	ft_string_handle(line, new_line);
+	ft_string_handle_2(new_line, new_line_2);
+	printf("this is the line 1 :%s:\n", new_line);
+	printf("this is the line 2 :%s:\n", new_line_2);
+	if (!new_line_2)
+		printf("🚫 in-bash: syntax error near unexpected token `\n");
+	else if (ft_check_close(new_line_2) == 1)
+	{
+		free(new_line);
+		free(new_line_2);
+		return (NULL);
+	}
+	free(new_line);
+	return (new_line_2);
 }
