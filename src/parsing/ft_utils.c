f5cd8053987f0c29662cca649c44c4b561ad9abf
martinzx13 <juan.martinxz43@gmail.com>
2024-03-13 13:28:08 +0000

    (fix) add a free memory for the line leacks

--- src/parsing/ft_utils.c
+++ src/parsing/ft_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juan-pma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 09:21:34 by juan-pma          #+#    #+#             */
-/*   Updated: 2024/02/21 10:49:44 by juan-pma         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:27:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,7 @@ char *ft_strcpy(char *dest, const char *src);
     🚩 check this function as we are alocating
     memmory here.
 */
-// t_cmd *cmdnew(char *args)
-// {
-//     t_cmd *comands;
-//     // char **newarg;
-//     int len;
-//     int i;
 
-//     i = -1;
-//     len = 0;
-//     comands = (t_cmd *)ft_calloc(sizeof(t_cmd), 1);
-//     // newarg = ft_split(args, '2');
-
-//     // while (newarg[len] != NULL)
-//     //     len++;
-//     // char **modified_list = (char **)malloc((len * 5) * sizeof(char *));
-//     // while (++i < len)
-//     // {
-//     //     modified_list[i] = (char *)malloc((ft_strlen(newarg[i]) + 1) * sizeof(char));
-//     //     ft_strcpy(modified_list[i], newarg[i]);
-//     // }
-//     comands->args = modified_list;
-//     comands->next = NULL;
-//     comands->redir = NULL;
-//     return (comands);
-// }
-
-// void cmdinback(t_cmd **comands, char *args)
-// {
-//     t_cmd *ptr;
-//     t_cmd *last;
-
-//     last = cmdnew(args);
-//     if (last == NULL)
-//         return;
-//     ptr = *(comands);
-//     while (ptr->next != NULL)
-//     {
-//         ptr = ptr->next;
-//     }
-//     ptr->next = last;
-// }
 char *ft_strcpy(char *dest, const char *src)
 {
     if (!src)
@@ -84,7 +44,6 @@ static void ft_preline(char *line)
             ft_memmove(&line[i], &line[i + 1], ft_strlen(&line[i + 1]) + 1);
         }
     }
-    // printf("line without $ :%s:\n", line);
 }
 
 void ft_checker_double_single(char *str, char quote)
@@ -109,7 +68,6 @@ void ft_checker_double_single(char *str, char quote)
         *dest++ = *src++;
     }
     *dest = '\0';
-    // printf("this is the dest :%s:\n", dest);
 }
 void ft_checker_quotes(char *str)
 {
@@ -133,7 +91,6 @@ void ft_checker_quotes(char *str)
         }
         src++;
     }
-    // printf("this is the dest :%s:\n", src);
 }
 
 void ft_quotes_remove(t_word_list *word_list)
@@ -186,10 +143,11 @@ int ft_whitespace(char *line)
 void ft_line_handler(t_line *line, t_env *cpyenv)
 {
     if (cpyenv == NULL)
-        line->value_env = "non-env@user";
-    else 
+        line->value_env = ft_strdup("non-env@user");
+    else
         line->value_env = ft_path_handler(cpyenv, "USER");
     line->color_line = ft_strjoin(ANSI_COLOR_CYAN, (line->value_env));
     line->line_text = ft_strjoin(line->color_line,
                                  "@🐧shell:$ " ANSI_COLOR_RESET);
+    free(line->value_env);
 }
