e9bc3a9250b0e6a8e0af6002f2b8604e24586c7b
martinzx13 <juan.martinxz43@gmail.com>
2024-03-18 14:33:31 +0000

    (add) the initialization t_cmd, and t_redir structure

--- src/parsing/ft_init_manager.c
+++ src/parsing/ft_init_manager.c
@@ -10,12 +10,14 @@ t_line *ft_init_manager(t_line *line)
     return line;
 }
 
-t_word_lists	*ft_init_word_list(t_word_lists *word_lists, char *token)
-{
-	word_lists = ft_calloc(1, sizeof(t_word_lists));
-	word_lists->current_token = NULL;
-	word_lists->node = NULL;
-	word_lists->head = NULL;
-	word_lists->subtoken = ft_strtok(token, "2");
-	return (word_lists);
-}
+// t_word_lists	*ft_init_word_list(char *token)
+// {
+// 	t_word_lists *word_lists;
+
+// 	word_lists = ft_calloc(1, sizeof(t_word_lists));
+// 	word_lists->current_token = NULL;
+// 	word_lists->node = NULL;
+// 	word_lists->head = NULL;
+// 	word_lists->subtoken = ft_strtok(token, "2");
+// 	return (word_lists);
+// }
