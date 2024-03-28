4646d3929b362515f257aa856aed02ff26c08193
diegosou4 <diegoaguia31@gmail.com>
2024-03-28 16:15:25 +0000

    (add) organize fuctions

---
+++ src/scomands/ft_cd2.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 16:11:00 by diegmore          #+#    #+#             */
+/*   Updated: 2024/03/28 16:11:02 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../../includes/mini.h"
+
+char *get_valuepwd(t_env **env, char *value)
+{
+    t_env *ptr;
+    char *key;
+    char *val;
+    if(env == NULL)
+        return(NULL);
+    ptr = (*env);
+    key  = ft_strdup(value);
+    while(ptr != NULL)
+    {
+        if(ft_strncmp(ptr->key,key,ft_strlen(key)) == 0)
+        {   
+            val = ft_strdup(ptr->value);
+            return(val);
+        }
+        ptr = ptr->next;
+    }
+    return(NULL);
+}
